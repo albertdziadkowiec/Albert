9eb23e5bedb662bd2b19cc02a88b87b61874b56b
albertdziadkowiec <55201574+albertdziadkowiec@users.noreply.github.com>
2019-11-14 15:15:06 -0600

    Create mssql.sql

---
+++ mssql.sql
@@ -0,0 +1,15 @@
+CREATE TABLE customer
+(
+CustomerID int,
+LastName varchar(50),
+FirstName varchar(50),
+EmailAddress varchar(50),
+Phone varchar(50),
+StreetAddress varchar(50),
+City` varchar(50),
+State` varchar(50),
+Zip` varchar(50),
+GPA` varchar(3)
+);
+
+
