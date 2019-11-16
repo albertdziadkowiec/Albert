d6583540931031648da23e7d83ab3d426efceedd
albertdziadkowiec <55201574+albertdziadkowiec@users.noreply.github.com>
2019-11-15 18:37:59 -0600

    Update DBP6.sql

--- DBP6.sql
+++ DBP6.sql
@@ -14,3 +14,9 @@ employee.EmployeeID
 
 This shows which employee ID was not on an invoice
 SELECT DISTINCT employeeID, LastName, FirstName FROM employee WHERE NOT EXISTS ( SELECT * FROM invoice WHERE invoice.EmployeeID = employee.EmployeeID )
+
+SELECT
+FROM ON
+ProjectName, FirstName, LastName, HoursWorked EMPLOYEE AS E JOIN ASSIGNMENT AS A
+E.EmployeeNumber = A.EmployeeNumber JOIN PROJECT AS P ON
+ORDER BY P.ProjectID, A.EmployeeNumber;
