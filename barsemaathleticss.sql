729f76d8afcc7e672a4e8d4805130b75242d8972
albertdziadkowiec <55201574+albertdziadkowiec@users.noreply.github.com>
2019-11-19 13:05:53 -0600

    Update barsemaathleticss.sql

--- barsemaathleticss.sql
+++ barsemaathleticss.sql
@@ -1,2 +1,5 @@
 ALTER TABLE `invoice_product`
   ADD CONSTRAINT `INV_PRO_INV_FK` FOREIGN KEY (`InvoiceNumber`) REFERENCES `invoice` (`InvoiceNumber`);
+  
+  ALTER TABLE `vendor`
+  ADD CONSTRAINT `FK_productnumber` FOREIGN KEY (`ProductNumber`) REFERENCES `product` (`ProductNumber`);
