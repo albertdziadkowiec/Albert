38722b48f3f863de1a94788c96ef767c155f043c
albertdziadkowiec <55201574+albertdziadkowiec@users.noreply.github.com>
2019-11-19 13:13:11 -0600

    Update barsemaathleticss.sql

--- barsemaathleticss.sql
+++ barsemaathleticss.sql
@@ -3,3 +3,5 @@ ALTER TABLE `invoice_product`
   
   ALTER TABLE `vendor`
   ADD CONSTRAINT `FK_productnumber` FOREIGN KEY (`ProductNumber`) REFERENCES `product` (`ProductNumber`);
+  
+  insert into Vendor (VendorID, VendorName, ProductName, ProductNumber, Phone) values (1, 'Barsema Athletics', 'AXAS', 1, '914-237-9148');
